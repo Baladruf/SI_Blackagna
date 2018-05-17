5202a0bc65b42624b14a628776bb75707616d630
Unknown <e.escher@rubika-edu.com>
2018-05-17 23:41:02 +0200

    begin anime

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -38,6 +38,8 @@ public class PlayerMovement : MonoBehaviour {
             return;
         }
 
+        //print();
+        playerController.animator.SetFloat("move", Mathf.Max(Mathf.Abs(playerController.player.GetAxis("MoveHorizontal")), Mathf.Abs(playerController.player.GetAxis("MoveVertical"))));
         playerController.rb.MovePosition(transform.position + new Vector3(playerController.player.GetAxis("MoveHorizontal"), 0, playerController.player.GetAxis("MoveVertical")) * (moveSpeed + (bonusSpeed * playerController.rankBonus)) * Time.deltaTime);
 
     }
