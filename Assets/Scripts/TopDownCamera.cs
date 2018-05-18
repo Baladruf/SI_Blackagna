e42f3098b4eaae58a5f88ec1653aecfa578a76bf
Devillers Lucas <devillerslucas@gmail.com>
2018-05-18 14:08:05 +0200

    Camera

---
+++ Assets/Scripts/TopDownCamera.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class TopDownCamera : MonoBehaviour
+{
+	public Transform Target;
+	public float Height = 10.0f;
+	public float Damping = 2.0f;
+
+	void LateUpdate ()
+	{
+		float wantedX = Target.position.x;
+		float wantedZ = Target.position.z;
+
+		float currentX = transform.position.x;
+		float currentZ = transform.position.z;
+		
+		currentX = Mathf.Lerp(currentX, wantedX, Damping * Time.deltaTime);
+		currentZ = Mathf.Lerp(currentZ, wantedZ, Damping * Time.deltaTime);
+
+		transform.position = new Vector3 (currentX, Height, currentZ);
+	}
+}
